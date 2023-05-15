cdb3c691807f9fe38e5375175dc7db5de0cde134
MariaGromova <marygromova10@gmail.com>
2023-05-16 00:26:42 +0200

    adding firebase logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'views/welcome_screen.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const EcommerceApp());
 }
-
 
 class EcommerceApp extends StatelessWidget {
   const EcommerceApp({super.key});
@@ -13,9 +14,8 @@ class EcommerceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: WelcomeScreen(),
-      
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
     );
   }
 }
--- lib/views/ui_additional_widgets.dart/banner_widget.dart
+++ lib/views/ui_additional_widgets.dart/banner_widget.dart
@@ -1,29 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_app/views/ui_additional_widgets.dart/colors.dart';
 import 'package:flutter/material.dart';
 
-class BannerWidget extends StatelessWidget {
+class BannerWidget extends StatefulWidget {
   const BannerWidget({super.key});
 
   @override
+  State<BannerWidget> createState() => _BannerWidgetState();
+}
+
+class _BannerWidgetState extends State<BannerWidget> {
+  @override
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final List _bannerImage = [];
+
+  getBanners() {
+    return _firestore
+        .collection('banners')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        setState(() {
+          _bannerImage.add(doc['image']);
+        });
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    getBanners();
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Container(
-        height: 140,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: elementsColor,
-          borderRadius: BorderRadius.circular(10)
-        ),
-
-        child: PageView(
-          children: [
-            Text('Banner 1'),
-            Text('Banner 2'),
-            Text('Banner 3'),
-          ],
-        ),
-      ),
+          height: 140,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: elementsColor, borderRadius: BorderRadius.circular(10)),
+          child: PageView.builder(
+            itemCount: _bannerImage.length,
+            itemBuilder: (context, index) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(_bannerImage[index], fit: BoxFit.cover,));
+          })),
     );
   }
 }
