ea6763d650f8ead205386dd0faa3ecca51016551
MariaGromova <marygromova10@gmail.com>
2023-05-30 23:52:30 +0300

    auth issues

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,13 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 class AuthController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<String> signUpUsers(
-    String email, String fullName, String phoneNumber, String password) async {
+  Future<String> signUpUsers(String email, String fullName, String phoneNumber,
+      String password) async {
     String res = 'Some error occurred';
 
     try {
@@ -18,19 +17,37 @@ class AuthController {
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
 
-        await _firestore.collection('buyers').doc(cred.user!.uid).set(
-            {'email': email, 
-            'fullName': fullName, 
-            'phoneNumber': phoneNumber,
-            'buyerId': cred.user!.uid,
-            'address': ''
-            });
+        await _firestore.collection('buyers').doc(cred.user!.uid).set({
+          'email': email,
+          'fullName': fullName,
+          'phoneNumber': phoneNumber,
+          'buyerId': cred.user!.uid,
+          'address': ''
+        });
         res = 'success';
       } else {
         res = 'Please, Fields must not be empty';
       }
     } catch (e) {}
 
+    return res;
+  }
+
+  loginUsers(String email, String password) async {
+    String res = 'something went wrong';
+
+    try {
+      if (email.isNotEmpty && password.isNotEmpty) {
+        await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+
+        res = 'success';
+      } else {
+        res = 'Fields must not be empty';
+      }
+    } catch (e) {
+      res = e.toString();
+    }
     return res;
   }
 }
--- lib/views/buyers/buyers_login_screen.dart
+++ lib/views/buyers/buyers_login_screen.dart
@@ -1,18 +1,22 @@
+import 'package:ecommerce_app/controllers/auth_controller.dart';
+import 'package:ecommerce_app/utils/show_snackbar.dart';
 import 'package:ecommerce_app/views/buyers/buyers_main_screen.dart';
 import 'package:ecommerce_app/views/ui_additional_widgets.dart/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../ui_additional_widgets.dart/border_clipper.dart';
 import '../ui_additional_widgets.dart/text_input.dart';
 
-class BuyersLoginScreen extends StatelessWidget {
+class BuyersLoginScreen extends StatefulWidget {
+  @override
+  State<BuyersLoginScreen> createState() => _BuyersLoginScreenState();
+}
 
+class _BuyersLoginScreenState extends State<BuyersLoginScreen> {
   late String email;
-  late String fullName;
-  late String phoneNumber;
   late String password;
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final AuthController _authController = AuthController();
 
   void _openHomePage(BuildContext context) {
     final newRoute = PageRouteBuilder<dynamic>(
@@ -27,6 +31,15 @@ class BuyersLoginScreen extends StatelessWidget {
     Navigator.pushAndRemoveUntil(context, newRoute, ModalRoute.withName(''));
   }
 
+  _loginUsers() async {
+    if (_formKey.currentState!.validate()) {
+      await _authController.loginUsers(email, password);
+      return showSnackbar(context, 'You are now logged in');
+    } else {
+      return showSnackbar(context, 'Fields must not be empty');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -34,6 +47,7 @@ class BuyersLoginScreen extends StatelessWidget {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       if (!resizeNotifier.value) resizeNotifier.value = true;
     });
+
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: GestureDetector(
@@ -59,99 +73,116 @@ class BuyersLoginScreen extends StatelessWidget {
               },
               child: SizedBox(
                 height: size.height,
-                child: Column(
-                  children: <Widget>[
-                    const Spacer(
-                      flex: 1,
-                    ),
-                    Stack(
-                      children: [
-                        const Align(
-                          alignment: Alignment.topCenter,
-                          child: _DragDownIndication(),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 105),
-                          child: ClipPath(
-                            clipper: InvertedTopBorderClipper(
-                              circularRadius: 40,
-                            ),
-                            child: Container(
-                              height: 360,
-                              width: double.infinity,
-                              color: Colors.white,
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 40),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  const SizedBox(height: 60),
-
-                                        TextFormField(
-          decoration: InputDecoration(
-              labelText: 'Email',
-              icon: Icon(Icons.alternate_email, color: elementsColor)),
-          onChanged: (value) {
-            email = value;
-          },
-        ),
-                                  const SizedBox(height: 20),
-                                         TextFormField(
-          decoration: InputDecoration(
-              labelText: 'Password',
-              icon: Icon(Icons.lock_outline, color: elementsColor)),
-          onChanged: (value) {
-            password = value;
-          },
-        ),
-                                  const SizedBox(height: 10),
-                                  TextButton(
-                                    onPressed: () {},
-                                    child: Text(
-                                      'Did you forget your password?',
-                                      style: TextStyle(
-                                        fontSize: 12,
-                                        color: elementsColor,
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  SizedBox(
-                                  
-                                    width: size.width * .65,
-                                    child: TextButton(
-                                      onPressed: () {
-                                        //  Navigator.of(context).push(MaterialPageRoute(builder: (context) => 
-                                        //  const BuyersMainScreen()));
-                                        
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      const Spacer(
+                        flex: 1,
+                      ),
+                      Stack(
+                        children: [
+                          const Align(
+                            alignment: Alignment.topCenter,
+                            child: _DragDownIndication(),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 105),
+                            child: ClipPath(
+                              clipper: InvertedTopBorderClipper(
+                                circularRadius: 40,
+                              ),
+                              child: Container(
+                                height: 360,
+                                width: double.infinity,
+                                color: Colors.white,
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 40),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    const SizedBox(height: 60),
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                          labelText: 'Email',
+                                          icon: Icon(Icons.alternate_email,
+                                              color: elementsColor)),
+                                      onChanged: ((value) {
+                                        email = value;
+                                      }),
+                                      validator: (value) {
+                                        if (value!.isEmpty) {
+                                          return 'Email fields must not be empty';
+                                        } else {
+                                          return null;
+                                        }
                                       },
-                                      style: TextButton.styleFrom(
-                                        foregroundColor: Colors.white,
-                                        padding: const EdgeInsets.all(12),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                        ),
-                                        backgroundColor: elementsColor,
-                                      ),
-                                      child: const Text(
-                                        'Log in',
+                                    ),
+                                    const SizedBox(height: 20),
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                          labelText: 'Password',
+                                          icon: Icon(Icons.lock_outline,
+                                              color: elementsColor)),
+                                      onChanged: (value) {
+                                        password = value;
+                                      },
+                                      validator: (value) {
+                                        if (value!.isEmpty) {
+                                          return 'Password must not be empty';
+                                        } else {
+                                          return null;
+                                        }
+                                      },
+                                    ),
+                                    const SizedBox(height: 10),
+                                    TextButton(
+                                      onPressed: () {},
+                                      child: Text(
+                                        'Did you forget your password?',
                                         style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w600,
+                                          fontSize: 12,
+                                          color: elementsColor,
                                         ),
                                       ),
                                     ),
-                                  )
-                                ],
+                                    const SizedBox(height: 10),
+                                    SizedBox(
+                                      width: size.width * .65,
+                                      child: TextButton(
+                                        onPressed: () {
+                                          _loginUsers();
+                                          //  Navigator.of(context).push(MaterialPageRoute(builder: (context) =>
+                                          //  const BuyersMainScreen()));
+                                        },
+                                        style: TextButton.styleFrom(
+                                          foregroundColor: Colors.white,
+                                          padding: const EdgeInsets.all(12),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          ),
+                                          backgroundColor: elementsColor,
+                                        ),
+                                        child: const Text(
+                                          'Log in',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
